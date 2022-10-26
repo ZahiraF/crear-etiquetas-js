48adad05e3c40be2f991ef13c4a43cc4223560a0
ZahiraF <fernandez.zahira@gmail.com>
2022-10-26 15:57:41 -0300

    apuntes

--- js/script.js
+++ js/script.js
@@ -6,3 +6,7 @@ function añadirEtiqueta(crear) {
     h1.innerHTML = crear;
     etiquetas.appendChild(h1);
 }
+
+//getElementById  -> seleccionar un elemento del documento por medio del valor del atributo id que se le haya asignado.
+//createElement  -> crea un elemento HTML especificado por su tagName , o un HTMLUnknownElement (en-US) si su tagName no se reconoce.
+//appendChild  -> Agrega un nuevo nodo al final de la lista de un elemento hijo de un elemento padre especificado.
